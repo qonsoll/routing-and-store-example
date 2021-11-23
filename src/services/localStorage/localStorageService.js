943d7c82a601cdcfd54df99530dede1675fada77
olexandermushtyn <mushtynolexander@gmail.com>
2021-11-23 18:29:50 +0200

    add local storage actions

--- src/services/localStorage/localStorageService.js
+++ src/services/localStorage/localStorageService.js
@@ -1,3 +1,20 @@
-const localStorageService = () => {}
+import _ from 'lodash'
+
+const localStorageService = () => {
+  const actions = {
+    get: (path) => JSON.parse(localStorage.getItem(path)),
+    update: (key, value) => localStorage.setItem(key, JSON.stringify(value)),
+    push: (key, path, value) => {
+      const currentValue = JSON.parse(localStorage.getItem(key))
+      console.log(currentValue)
+      Array.isArray(currentValue) && currentValue.push(value)
+      _.set(currentValue, path, value)
+      localStorage.setItem(key, JSON.stringify(currentValue))
+    },
+    remove: (path) => localStorage.removeItem(path)
+  }
+
+  return actions
+}
 
 export default localStorageService
