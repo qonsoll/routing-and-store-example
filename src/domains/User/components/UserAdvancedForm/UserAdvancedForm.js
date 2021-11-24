39a4e83ac5d2c26b79979fcbca2801e41292bc1e
olexandermushtyn <mushtynolexander@gmail.com>
2021-11-24 16:47:37 +0200

    Added local storage

--- src/domains/User/components/UserAdvancedForm/UserAdvancedForm.js
+++ src/domains/User/components/UserAdvancedForm/UserAdvancedForm.js
@@ -3,15 +3,12 @@ import { AddressSimpleForm } from 'domains/Address/components'
 import { InterestsList } from 'domains/Interest/components'
 import { useCreateRuntimeStorage } from 'services/qonsoll-data/RuntimeStorage'
 import { divide } from 'lodash'
-import { localStorageService } from 'services/localStorage'
 
 const UserAdvancedForm = ({ id }) => {
   const { store, document, storage } = useCreateRuntimeStorage({
     ordered: {},
     structured: {}
   })
-
-  const { get, update, push } = localStorageService()
 
   console.log('new store ->>', store)
 
@@ -38,15 +35,7 @@ const UserAdvancedForm = ({ id }) => {
       </button>
       <button onClick={() => st.update('test', 15)}>Try update storage</button>
       <button onClick={() => st.get('test')}>Try get storage</button>
-      <button onClick={() => update('test', { hello: 'asdasd' })}>
-        Local storage update test
-      </button>
-      <button onClick={() => console.log(get('test'))}>
-        Local storage get test
-      </button>
-      <button onClick={() => push('test', 'field', 'some value')}>
-        Local storage push test
-      </button>
+
       <UserSimpleForm title="General info" />
       <AddressSimpleForm title="Address" />
       <InterestsList title="Interests" />
--- src/services/localStorage/localStorageService.js
+++ src/services/localStorage/localStorageService.js
@@ -1,20 +1,3 @@
-import _ from 'lodash'
-
-const localStorageService = () => {
-  const actions = {
-    get: (path) => JSON.parse(localStorage.getItem(path)),
-    update: (key, value) => localStorage.setItem(key, JSON.stringify(value)),
-    push: (key, path, value) => {
-      const currentValue = JSON.parse(localStorage.getItem(key))
-      console.log(currentValue)
-      Array.isArray(currentValue) && currentValue.push(value)
-      _.set(currentValue, path, value)
-      localStorage.setItem(key, JSON.stringify(currentValue))
-    },
-    remove: (path) => localStorage.removeItem(path)
-  }
-
-  return actions
-}
+const localStorageService = () => {}
 
 export default localStorageService
