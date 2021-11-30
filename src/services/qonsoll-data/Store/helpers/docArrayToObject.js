82bca57b30314467e3affa65c33f626946af873f
Oleksiy Pastukhov <oleksiy.pastukhov@senseteq.io>
2021-11-30 12:53:58 +0200

    Added part of findAll

---
+++ src/services/qonsoll-data/Store/helpers/docArrayToObject.js
@@ -0,0 +1,11 @@
+const docArrayToObject = (docs, customValue) => {
+  const result = {}
+  if (docs) {
+    docs.forEach((doc) => {
+      result[doc.id || doc.data.id] = customValue || doc
+    })
+  }
+  return result
+}
+
+export default docArrayToObject
