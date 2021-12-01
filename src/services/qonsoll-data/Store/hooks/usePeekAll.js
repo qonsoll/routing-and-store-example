27a87582cf54f8eedfbf57c327a5ba4a3bbf5d30
olexandermushtyn <mushtynolexander@gmail.com>
2021-12-01 12:56:41 +0200

    delete unused files

--- src/services/qonsoll-data/Store/hooks/usePeekAll.js
+++ src/services/qonsoll-data/Store/hooks/usePeekAll.js
@@ -3,6 +3,12 @@ import { peekAll } from '../methods'
 
 const usePeekAll = (query, config) => {
   const { runtimeStorage, defaultAdapter, models } = useStore()
+  const jsonData = JSON.parse(
+    '{"users":{"7WB6kbZSPbrzuJJlmOwQ":{"firstName":"Yevhen","lastName":"Bogdanov1","id":"7WB6kbZSPbrzuJJlmOwQ","birthDate":null,"age":"30","public":true,"interests":["interest1","interest2","interest3"],"address":"address1"}},"addresses":{"address1":{"country":"country1","city":"city1","id":"address1"}},"cities":{"city1":{"id":"city1","name":"Khmelnitskiy"}},"countries":{"country1":{"id":"country1","name":"Ukraine"}},"interests":{"interest1":{"id":"interest1","name":"JS"},"interest2":{"id":"interest2","name":"MongoDB"},"interest3":{"name":"NodeJS","id":"interest3"}}}'
+  )
+
+  runtimeStorage.state = jsonData
+  console.log(runtimeStorage)
 
   peekAll(query, runtimeStorage, models)
 }
--- src/services/qonsoll-data/Store/methods/peekAll.js
+++ src/services/qonsoll-data/Store/methods/peekAll.js
@@ -2,15 +2,15 @@ import { graphQlQueryToJson } from 'graphql-query-to-json'
 import { buildPeekCommandsStack, execPeekCommands } from '../helpers'
 
 const peekAll = (query, runtimeStorage, models) => {
-  //   const queryJSON = graphQlQueryToJson(query)
-  //   const commands = buildPeekCommandsStack(queryJSON.query, 'findAll')
-  //   console.log('peekAll commandStack ---->', commands)
+  const queryJSON = graphQlQueryToJson(query)
+  const commands = buildPeekCommandsStack(queryJSON.query, 'findAll')
+  console.log('peekAll commandStack ---->', commands)
 
-  //   const result = execPeekCommands(commands, runtimeStorage, models)
-  //   console.log(
-  //     'peekResult ------> 🚀 ~ file: findAll.js ~ line 10 ~ findAll ~ result',
-  //     result
-  //   )
+  const result = execPeekCommands(commands, runtimeStorage, models)
+  console.log(
+    'peekResult ------> 🚀 ~ file: findAll.js ~ line 10 ~ findAll ~ result',
+    result
+  )
   console.log('runtimeStorage ---->', runtimeStorage)
 }
 
