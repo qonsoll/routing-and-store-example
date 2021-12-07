a98c7d7599204a765b6e7f809339337c64ffc562
olexandermushtyn <mushtynolexander@gmail.com>
2021-12-07 17:51:45 +0200

    add useQueries hook

--- src/services/qonsoll-data/Store/hooks/useQueries.js
+++ src/services/qonsoll-data/Store/hooks/useQueries.js
@@ -1,3 +1,7 @@
-const useQueries = () => {}
+import { usePeekAll, usePeekRecord, useFindAll, useFindRecord } from '../hooks'
+
+const useQueries = () => {
+  return { usePeekAll, usePeekRecord, useFindAll, useFindRecord }
+}
 
 export default useQueries
