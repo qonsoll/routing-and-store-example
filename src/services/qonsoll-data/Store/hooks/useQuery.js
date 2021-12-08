730dff01e45f6540a08f92172418c41693e7f308
olexandermushtyn <mushtynolexander@gmail.com>
2021-12-08 16:03:10 +0200

    inital useQueries

--- src/services/qonsoll-data/Store/hooks/useQuery.js
+++ src/services/qonsoll-data/Store/hooks/useQuery.js
@@ -1,3 +1,17 @@
-const useQuery = () => {}
+import useStore from '../useStore'
+import { peekAll, construct, queries } from '../methods'
+import { useState, useEffect } from 'react'
+
+const useQuery = (query, config) => {
+  const { runtimeStorage, adapter, models } = useStore()
+  const [error] = useState(null)
+  const [documents, setDocuments] = useState([])
+
+  useEffect(() => {
+    const queriesFetcher = async () => {
+      const dbData = await queries({ query, runtimeStorage, models })
+    }
+  }, [])
+}
 
 export default useQuery
