938f9e167cc010364225816d33944fc47d4fdf99
olexandermushtyn <mushtynolexander@gmail.com>
2021-12-13 18:28:50 +0200

    add comments to useFetchAll and useFetchRecord hooks

--- src/services/qonsoll-data/Store/hooks/useFetchAll.js
+++ src/services/qonsoll-data/Store/hooks/useFetchAll.js
@@ -2,22 +2,36 @@ import useStore from '../useStore'
 import { fetchAll, construct } from '../methods'
 import { useState, useEffect } from 'react'
 
+/**
+ * Method helps to fetch all data by query from DB
+ * @param {string} query graphql like query
+ * @param {object} options options object (fetchInterval: number, forceIntervalRefresh: boolean, construct: boolean)
+ * @param {string} disable property that disable fetching data
+ * @returns [documents, loading, error]
+ */
 const useFetchAll = (query, config, disabled) => {
+  // Extracting adapter and models from StoreContext
   const { defaultAdapter, models } = useStore()
+
+  // Values (state) that should be returned
   const [error] = useState(null)
   const [documents, setDocuments] = useState([])
+  const [loading, setLoading] = useState(false)
 
   useEffect(() => {
+    // Method that helps to fetch data from DB
     const allFetcher = async () => {
+      setLoading(true)
       const dbData = await fetchAll({ query, adapter: defaultAdapter, models })
 
       const constructedData = construct(dbData, query, models)
       setDocuments(constructedData)
+      setLoading(false)
     }
     if (!disabled) allFetcher()
   }, [models, query, defaultAdapter, disabled])
 
-  return [documents, error]
+  return [documents, loading, error]
 }
 
 export default useFetchAll
--- src/services/qonsoll-data/Store/hooks/useFetchRecord.js
+++ src/services/qonsoll-data/Store/hooks/useFetchRecord.js
@@ -2,16 +2,30 @@ import useStore from '../useStore'
 import { fetchRecord, construct } from '../methods'
 import { useState, useEffect } from 'react'
 
+/**
+ * Method helps to fetch record data by query from DB
+ * @param {string} query graphql like query
+ * @param {object} options options object (fetchInterval: number, forceIntervalRefresh: boolean, construct: boolean)
+ * @param {string} disable property that disable fetching data
+ * @returns [documents, loading, error]
+ */
 const useFetchRecord = (query, config, disabled) => {
+  // Extracting adapter and models from StoreContext
   const { defaultAdapter, models } = useStore()
+
+  // Values (state) that should be returned
   const [error] = useState(null)
+  const [loading, setLoading] = useState(false)
   const [document, setDocuments] = useState([])
 
   useEffect(() => {
+    // Method that helps to fetch record data from DB
     const recordFetcher = async () => {
+      setLoading(true)
       const data = await fetchRecord({ query, adapter: defaultAdapter, models })
       const constructedData = construct(data, query, models)
       setDocuments(constructedData)
+      setLoading(false)
     }
     if (!disabled) recordFetcher()
   }, [models, query, defaultAdapter, disabled])
